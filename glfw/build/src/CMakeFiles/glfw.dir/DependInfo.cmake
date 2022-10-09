d5df83029f69df3727c53ffb4d82a17387cbf689
liumingming6 <liumingming6@xiaomi.com>
2022-10-09 19:18:46 +0800

    msg

---
+++ glfw/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mi/myCode/OpenGLtest/glfw/src/context.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/egl_context.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/glx_context.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/init.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/input.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/linux_joystick.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/monitor.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/null_init.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/null_init.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/null_joystick.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/null_joystick.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/null_monitor.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/null_monitor.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/null_window.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/null_window.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/osmesa_context.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/platform.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/platform.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/posix_module.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/posix_module.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/posix_poll.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/posix_poll.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/posix_thread.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/posix_time.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/vulkan.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/window.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/x11_init.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/x11_monitor.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/x11_window.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/mi/myCode/OpenGLtest/glfw/src/xkb_unicode.c" "/home/mi/myCode/OpenGLtest/glfw/build/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_DEFAULT_SOURCE"
+  "_GLFW_X11"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
